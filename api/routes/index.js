07c7144aea82769c7eaf7dbf39d1f42c2544f4ab
uci-mars <chheangm@uci.edu>
2020-02-06 17:19:48 -0800

    attached express backend api to repo

---
+++ api/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch');
+const WebSocAPI = require('websoc-api');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/websoc/:department/:courseNum', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const opts = {
+    term: '2020 Winter',
+
+    department: req.params.department,
+    courseNumber: req.params.courseNum
+}
+  const result = WebSocAPI.callWebSocAPI(opts);
+  result.then((json) => res.json(json));
+});
+
+module.exports = router;
