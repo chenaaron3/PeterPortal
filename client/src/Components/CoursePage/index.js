fab78ce75808de4c7b102ec1d87012b794a0281a
uci-mars <chheangm@uci.edu>
2020-02-08 00:18:12 -0800

    changed elasticsearch field from number -> id based on new es index format

--- client/src/Components/CoursePage/index.js
+++ client/src/Components/CoursePage/index.js
@@ -48,20 +48,10 @@ class CoursePage extends React.Component {
         method: "GET"
         };
 
-      // fetch("http://localhost:3000/websoc/" + queryParams[0] + "/" + queryParams[1], requestHeader).then(data => {return data.json()}).then(res => this.setState({WebSocData: res}));
+
       fetch("http://localhost:3001/websoc/" + queryParams.slice(0, -1).join(" ") + "/" + queryParams[queryParams.length - 1], requestHeader).then(data => {return data.json()})
       .then(res => this.setState({WebSocData: res})).catch(() => {console.log("no course found")});
 
-      // if (queryParams.length === 2){
-      //   fetch("http://localhost:3001/websoc/" + queryParams[0] + "/" + queryParams[1], requestHeader).then(data => {return data.json()})
-      //   .then(res => this.setState({WebSocData: res})).catch(() => {console.log("no course found")});
-      // }
-      // else {
-      //   fetch("http://localhost:3001/websoc/" + queryParams[0] + " " + queryParams[1] + "/" + queryParams[2], requestHeader).then(data => {return data.json()})
-      //   .then(res => this.setState({WebSocData: res})).catch(() => {console.log("no course found")});
-      // }
-      
-      
   }
 
   parseWebSocData() {
@@ -82,7 +72,7 @@ class CoursePage extends React.Component {
       
       return (    
         <div style={{width: "800px", margin: "auto"}}><h1>{this.state.courseData.number}</h1>
-        <h2>{this.state.courseData.name}</h2>
+        <h2>{this.state.courseData.id}</h2>
         <h3>{this.state.courseData.department}</h3>
         {this.state.courseData.description}<br/><br/>
         {this.state.courseData.prerequisite}
