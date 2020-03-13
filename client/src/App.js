0cdf03dd69396de7264832107e92b0072e91f59b
rhartant <rhartant@uci.edu>
2020-03-12 18:11:32 -0700

    Header Bar
    Made the header bar. It's sticky.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SearchPage from "./Components/SearchPage";
 import CoursePage from "./Components/CoursePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ReactiveBase, DataSearch } from "@appbaseio/reactivesearch";
-import { Input, Menu } from 'semantic-ui-react';
+import { Input, Menu, Sticky } from 'semantic-ui-react';
 import ElasticCloudInfo from "./ElasticCloudInfo";
 
 class App extends React.Component {
@@ -17,41 +17,39 @@ class App extends React.Component {
 
     return (
       <ReactiveBase app="courses" url={ElasticCloudInfo.elasticEndpointURL}>
+
       <Router>
-        <Menu secondary>
-          <Menu.Item
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name="messages"
-            active={activeItem === "messages"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name="friends"
-            active={activeItem === "friends"}
-            onClick={this.handleItemClick}
-          />
-          
-            <Menu.Item>
-            <DataSearch
-              style={{ width: "100%", margin: "auto" }}
-              componentId="q"
-              dataField={["dept_alias", "id", "description"]}
-              autosuggest={false}
-              URLParams={true}
-            />
-            </Menu.Item>
-            <Menu.Menu position="right">
-            <Menu.Item
-              name="logout"
-              active={activeItem === "logout"}
+        <Sticky>
+          <Menu secondary color={"blue"} inverted>     
+            
+            <Menu.Item 
+              name="PeterPortal"
+              active={activeItem === "peterPortal"}
               onClick={this.handleItemClick}
             />
-          </Menu.Menu>
-        </Menu>
+
+
+            <div style={ {marginLeft: "170px" }}>
+            <Menu.Item>
+              <DataSearch
+                style={{ width: "100%", margin: "auto" }}
+                componentId="q"
+                dataField={["dept_alias", "id", "description"]}
+                autosuggest={false}
+                URLParams={true}
+              />
+            </Menu.Item>
+            </div>
+            <Menu.Menu position="right">
+              <Menu.Item
+                name="logout"
+                active={activeItem === "logout"}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu>
+        </Sticky>
+
         {/* <h5 style={{textAlign: "center", color: "gray"}}>UCI Catalogue Search - alpha v1.0</h5>
           <h5 style={{textAlign: "center", color: "light-gray"}}><a href="https://github.com/icssc-projects/uci-catalogue-search/issues/new">If you see something, say something!</a></h5> */}
         <Switch>
