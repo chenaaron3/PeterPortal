75f7a8f07fffe58c81ffe91419528fa63a72a734
Yizhen Liu <imliuyzh@outlook.com>
2020-04-06 11:42:35 -0700

    Add dropdown menus to grade distribution section

--- client/src/Components/CoursePage/CoursePage.js
+++ client/src/Components/CoursePage/CoursePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Timetable from "../Timetable";
 import ReviewsModule from "../ReviewsModule";
 import PrereqTree from "../PrereqTree";
-import GradeDist from "./GradeDist.js";
+import GradeDist from "../GradeDist";
 import { Grid, Icon, Divider, Card, Radio } from "semantic-ui-react";
 import "./CoursePage.scss";
 
@@ -115,7 +115,7 @@ class CoursePage extends React.Component {
               </Card>
             </Grid.Row>
             
-            <GradeDist/>
+            <GradeDist />
 
             {/* <Grid.Row className="course_content-container course_prereq-tree-container">
               <Card>
