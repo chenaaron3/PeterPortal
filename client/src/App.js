240d2fcd7e5b503e26cf8492605aa3858712dc5a
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-05-08 14:08:19 -0700

    separated course and professor to 2 pages

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,17 @@ class App extends React.Component {
 
   render() {
     return (
-        <Router>
-          <NavBar/>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/search" />
-            </Route>
-            <Route path="/search" component={SearchPage} />
-            <Route path="/course/:id" component={CoursePage} />
-            <Route path="/professor/:id" component={ProfessorPage} />
-          </Switch>
-        </Router>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/search/courses" />
+          </Route>
+          <Route path="/search/:index" component={SearchPage} />
+          <Route path="/course/:id" component={CoursePage} />
+          <Route path="/professor/:id" component={ProfessorPage} />
+        </Switch>
+      </Router>
     );
   }
 }
--- client/src/Components/SearchPage/SearchPage.js
+++ client/src/Components/SearchPage/SearchPage.js
@@ -1,22 +1,145 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SearchPage.scss";
-import SearchEngine from "./SearchEngine.js";
-import queryString from "query-string";
+import CourseFilter from "./CourseFilter.js";
+import ProfessorFilter from "./ProfessorFilter.js";
+import ProfessorHitItem from "./ProfessorHitItem.js";
+import CourseHitItem from "./CourseHitItem.js";
+import { Icon, Menu } from "semantic-ui-react";
+import {
+    Pagination,
+    SearchkitComponent,
+    Hits, HitsStats,
+    InitialLoader, SearchBox,
+    SearchkitManager, SearchkitProvider
+} from "searchkit";
 
-class SearchPage extends React.Component {
-  state = { activeItem: "courses" };
+const InitialLoaderComponent = props => <div>Fetching course data...</div>;
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+const customHitStats = props => (
+    <div>
+        <p style={{ textAlign: "right", fontSize: "8pt" }}>{props.hitsCount.value} results found in {props.timeTaken}ms</p>
+    </div>
+)
 
-  render() {
-    const { activeItem } = this.state;
-    return (
-      <>
-        {activeItem === "courses" && <SearchEngine activeItem={activeItem} handleItemClick={this.handleItemClick}/>}
-        {activeItem === "professors" && <SearchEngine activeItem={activeItem} handleItemClick={this.handleItemClick}/>}
-      </>
-    );
-  }
+class SearchPage extends SearchkitComponent {
+
+    queryFieldValues = {
+        "courses": [
+            "id_department^10",
+            "id_number^10",
+            "description",
+            "dept_alias^10",
+            "name^3",
+        ],
+        "professors": [
+            "name^10",
+            "ucinetid^10",
+            "title^3",
+            "courseHistory",
+            "department^3",
+        ]
+    }
+    highlightFieldValues = {
+        "courses": [
+            "name",
+            "id_department",
+            "id_number",
+            "description",
+        ],
+        "professors": [
+            "name",
+            "title",
+            "department",
+            "courseHistory"
+        ]
+    }
+
+    render() {
+        let searchkit = new SearchkitManager("/" + this.props.match.params.index);
+        return (
+            <SearchkitProvider searchkit={searchkit}>
+                <div className="App" style={{ display: "flex", flexDirection: "row" }}>
+                    <div className="search-page">
+                        <div className="sidebar-container">
+                            <Menu pointing secondary vertical className="mode-switcher">
+                                <a href="/search/courses">
+                                    <Menu.Item
+                                        name="courses"
+                                        icon="book"
+                                        active={this.props.match.params.index === "courses"}
+                                    />
+                                </a>
+                                <a href="/search/professors">
+                                    <Menu.Item
+                                        name="professors"
+                                        icon="graduation cap"
+                                        active={this.props.match.params.index === "professors"}
+                                    />
+                                </a>
+                            </Menu>
+
+                            {this.props.match.params.index == "courses" && <CourseFilter />}
+                            {this.props.match.params.index == "professors" && <ProfessorFilter />}
+
+                        </div>
+
+                        <div className="search-pane-container">
+
+                            <div className="searchbox-container">
+                                <div style={{ display: "flex" }}>
+                                    <Icon name='search' size='large' className="search-icon" />
+                                    <SearchBox
+                                        autofocus={true}
+                                        searchOnChange={true}
+                                        queryFields={this.queryFieldValues[this.props.match.params.index]}
+                                        searchThrottleTime={300}
+                                        placeholder={this.props.match.params.index == "courses" ? "Course number, title and description" : "Professor name, title, and department"}
+                                    />
+                                </div>
+
+                                <div>
+                                    <HitsStats component={customHitStats} />
+                                </div>
+                            </div>
+
+
+                            <div style={{ minHeight: "100vh" }}>
+                                <Hits
+                                    itemComponent={this.props.match.params.index == "courses" ? CourseHitItem : ProfessorHitItem}
+                                    hitsPerPage={20}
+                                    highlightFields={this.highlightFieldValues[this.props.match.params.index]}
+                                    customHighlight={{
+                                        pre_tags: ["<highlight>"],
+                                        post_tags: ["</highlight>"],
+                                    }}
+                                />
+                            </div>
+
+                            <InitialLoader component={InitialLoaderComponent} />
+
+                            <Pagination showNumbers={true} />
+
+                            <footer class="footer">
+                                <div>
+                                    <a href="https://github.com/icssc-projects">Github</a>
+                                    <a href="/">API</a>
+                                    <a href="/">About</a>
+                                    <a href="/">Team</a>
+                                    <a href="/">FAQ</a>
+                                </div>
+                                <div class="copyright">
+                                    <p>Made with ♥ by <a href="https://studentcouncil.ics.uci.edu/">ICSSC Project Committee</a></p>
+                                </div>
+                            </footer>
+
+                        </div>
+                    </div>
+                </div>
+            </SearchkitProvider>
+        );
+    }
+
 }
 
 export default SearchPage;
