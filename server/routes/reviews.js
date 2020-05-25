ca241d10948ac79c5f16f35420ce5f448ad8bd96
Raman <ramanxg@gmail.com>
2020-05-25 15:27:59 -0700

    added badges routes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -7,25 +7,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource reviews');
 });
 
-router.post("/post", function(req, res, next){
-  console.log("Hi Poster!", JSON.stringify(req.body));
-})
-
+//GET reviews for given professor
 router.get('/professor', function(req, res, next)  {
   let sql = `SELECT * FROM reviews AS r WHERE r.prof_id = ${escape(req.query.profID)} AND r.pub_status != 'removed' ORDER BY r.submitted_at DESC`
   executeQuery(sql, function(results) {
     res.json(results);
   });
-})
+});
 
+//GET reviews for given course
 router.get('/course', function(req, res, next)  {
   let sql = `SELECT * FROM reviews AS r WHERE r.course_id = ${escape(req.query.courseID)} AND r.pub_status != 'removed' ORDER BY r.submitted_at DESC`
-
   executeQuery(sql, function(results) {
     res.json(results);
   });
-})
+});
 
+
+//POST a new review
 router.post('/addReview', function(req, res) {
   const data = {
     text: req.body.text,
@@ -43,32 +42,31 @@ router.post('/addReview', function(req, res) {
   let sql = `INSERT INTO reviews 
   (body, rating, difficulty, user_id, course_id, prof_id, submitted_at, grade, taken_in, pub_status)
   VALUES( ${escape(data.text)}, ${data.rating}, ${data.difficulty}, ${data.userID}, ${escape(data.courseID)}, ${escape(data.profID)}, ${escape(data.date)}, ${escape(data.grade)}, ${escape(data.takenIn)}, ${escape(data.pubStatus)})`
-
   executeQuery(sql, function(results) {
     res.json(results);
   });
 })
 
+//upvote a review
+//upvote will succeed if not yet upvoted
+//upvote is removed if already upvoted
+//upvote will do nothing if downvoted
 router.put('/upVoteReview', function(req, res) {
 
   let sql = `SELECT * FROM votes WHERE user_id=${req.user.userID} AND review_id=${req.body.reviewID}`
-
   executeQuery(sql, function(results) {
     //if there is no vote
     if (results.length == 0) {
       sql = `UPDATE reviews SET up_votes = up_votes + 1 WHERE id = ${req.body.reviewID};
       INSERT INTO votes VALUES(${req.user.userID}, ${req.body.reviewID}, true);`
-
       executeQuery(sql, function(results) {
         res.json(results);
       });
     } else if (!results[0].up) {
-      //trying to upvote but it has been downvoted
       res.send("You already downvoted the review.")
     } else { //if it is a upvote delete it
       sql = `UPDATE reviews SET up_votes = up_votes - 1 WHERE id = ${req.body.reviewID};
       DELETE FROM votes WHERE user_id=${req.user.userID} AND review_id=${req.body.reviewID}`
-
       executeQuery(sql, function(results) {
         res.json(results);
       });
@@ -76,6 +74,11 @@ router.put('/upVoteReview', function(req, res) {
   });
 });
 
+
+//downvote a review
+//downvote will succeed if not yet downvoted
+//downvote is removed if already downvote
+//downvote will do nothing if upvoted
 router.put('/downVoteReview', function(req, res) {
   let sql = `SELECT * FROM votes WHERE user_id=${req.user.userID} AND review_id=${req.body.reviewID}`
 
@@ -84,18 +87,15 @@ router.put('/downVoteReview', function(req, res) {
     if (results.length == 0) {
       sql = `UPDATE reviews SET down_votes = down_votes + 1 WHERE id = ${req.body.reviewID};
       INSERT INTO votes VALUES(${req.user.userID}, ${req.body.reviewID}, false);`
-
       executeQuery(sql, function(results) {
         res.json(results);
       });
     } else if (results[0].up) {
       //trying to downvote but it has been upvote
       res.send("You already upvoted the review.")
-
     } else { //if it is a downvote delete it
       sql = `UPDATE reviews SET down_votes = down_votes - 1 WHERE id = ${req.body.reviewID};
       DELETE FROM votes WHERE user_id=${req.user.userID} AND review_id=${req.body.reviewID};`
-
       executeQuery(sql, function(results) {
         res.json(results);
       });
@@ -108,7 +108,6 @@ router.post('/flagReview', function(req, res) {
   let sql = `INSERT INTO flagged 
   (user_id, review_id, reason, comments, reported_at, flag_status)
   VALUES(${req.user ? req.user.userID: "NULL"}, ${req.body.reviewID}, ${escape(req.body.reason)}, ${escape(req.body.comments)}, ${escape(req.body.date)}, 'pending')`
-
   executeQuery(sql, function(results) {
     res.json(results);
   });
